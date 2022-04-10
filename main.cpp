cdd379ecd3f45f67e261c639f1ba3d9a63b07b55
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-04-10 18:44:56 -0500

    added readme and main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CropFarming
+//
+//  Created by emilio on 4/10/22.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
