0e9871251110ab5a83a5e50425debd758c6dbe8b
e-mar404 <egmarinn@cougarnet.uh.edu>
2022-04-26 18:29:13 -0500

    metRequirements()

--- Crop.cpp
+++ Crop.cpp
@@ -17,39 +17,30 @@ Crop::Crop(){
     water = 8;
     sunLight = 5;
     daysWithSoil = 0;
-    disease = false;
+    bugs = 0;
 }
 
 
-Crop::Crop(int _health, int _daysAlive, int _water, int _sunLight, int _daysWithSoil, bool _disease){
+Crop::Crop(int _health, int _daysAlive, int _water, int _sunLight, int _daysWithSoil, int _bugs){
     health = _health;
     daysAlive = _daysAlive;
     water = _water;
     sunLight = _sunLight;
     daysWithSoil = _daysWithSoil;
-    disease = _disease;
+    bugs = _bugs;
 }
 
 void Crop::nextDay(){
-    int everyOtherDay = 0;
-    // Set seed for random component of game
     srand((unsigned int) time(0));
     
-    // Increase daysAlive
     this->setDaysAlive(this->getDaysAlive() + 1);
-    
-    // Decrease water
     this->decreaseWater(1);
-    
-    // Increase days with soil
     this->setDaysWithSoil(this->getDaysWithSoil() + 1);
-    
-    // Increase/decrease sunlight (-1 to 1)
     this->setSunLight(this->getSunLight() + ((rand() % 2) - 1));
     
-    //Randomly give disease{
-    if ((rand() % 3) == 1){
-        this->setDisease(true);
+    //Randomly give bugs{
+    if ((rand() % 6) == 1){
+        this->setBugs(this->getBugs() + 1);
     }
     
     // Check parameters for decreasing health
@@ -57,17 +48,9 @@ void Crop::nextDay(){
     if (!met){
         this->decreaseHealth(1);
     }
-    if (met && everyOtherDay % 2 == 1){
-        this->increaseHealth(1);
+    if (met){
+        this->increaseHealth(2);
     }
-    
-
-    
-    // Next Day message
-    std::cout << "Day " << this->getDaysAlive() << std::endl;
-    std::cout << "Good morning. ";
-    this->dailyReport();
-    
 }
 
 
