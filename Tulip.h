ff1b0db21600bff6fe4cab0e87494011e57ae958
Emilio Marin <98435584+e-mar404@users.noreply.github.com>
2022-04-28 18:50:52 -0500

    Add files via upload

---
+++ Tulip.h
@@ -0,0 +1,26 @@
+//
+//  Tulip.h
+//  CropFarming
+//
+//  Created by emilio on 4/12/22.
+//
+
+#ifndef Tulip_h
+#define Tulip_h
+
+class Tulip: public Crop{
+    
+    public:
+    // Constructor
+        Tulip(): Crop(){
+            water = 7;
+            sunLight = 9;
+        }
+    
+    // Get/set functions
+        bool metRequirements();
+        std::string getType() {return "Tulip";}
+
+}; 
+
+#endif /* Tulip_h */
